dde6961b20ce936114d4511687394552b26c1aa0
Kris <kreisnews@gmail.com>
2021-01-30 23:14:08 +0300

    Add List method for Storage struct to retrieve products from database

--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -252,3 +252,100 @@ func (s *Storage) Delete(ctx context.Context, merchantID int64, offerIDs []int64
 
 	return deleted, nil
 }
+
+type listParameters struct {
+	merchantID int64
+	offerID    int64
+	nameQuery  string
+}
+
+const (
+	defaultMerchantID = 0
+	defaultOfferID    = 0
+	defaultNameQuery  = ""
+)
+
+func (lp listParameters) isAnyNonDefault() bool {
+	return lp.merchantID == defaultMerchantID || lp.offerID == defaultOfferID || lp.nameQuery == defaultNameQuery
+}
+
+type ListOption func(parameters *listParameters)
+
+func WithMerchantID(id int64) ListOption {
+	return func(p *listParameters) {
+		p.merchantID = id
+	}
+}
+
+func WithOfferID(id int64) ListOption {
+	return func(p *listParameters) {
+		p.offerID = id
+	}
+}
+
+func WithNameQuery(q string) ListOption {
+	return func(p *listParameters) {
+		p.nameQuery = q
+	}
+}
+
+func (s *Storage) List(ctx context.Context, options ...ListOption) ([]Product, error) {
+	parameters := &listParameters{
+		merchantID: defaultMerchantID,
+		offerID:    defaultOfferID,
+		nameQuery:  defaultNameQuery,
+	}
+
+	for _, opt := range options {
+		opt(parameters)
+	}
+
+	var rows pgx.Rows
+	var err error
+
+	b := strings.Builder{}
+	b.WriteString("SELECT * FROM products")
+
+	if parameters.isAnyNonDefault() {
+		b.WriteString(" WHERE 1 = 1")
+
+		if parameters.merchantID != defaultMerchantID {
+			b.WriteString(" AND merchant_id = " + strconv.FormatInt(parameters.merchantID, 10))
+		}
+
+		if parameters.offerID != defaultOfferID {
+			b.WriteString(" AND offer_id = " + strconv.FormatInt(parameters.offerID, 10))
+		}
+
+		if parameters.nameQuery != defaultNameQuery {
+			b.WriteString(" AND name ^@ $1")
+			rows, err = s.db.Query(ctx, b.String(), parameters.nameQuery)
+		} else {
+			sql := b.String()
+			rows, err = s.db.Query(ctx, sql)
+		}
+	}
+
+	if err != nil {
+		s.logger.Error("Selecting rows", zap.Error(err))
+		return nil, err
+	}
+
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.MerchantID, &p.OfferID, &p.Name, &p.Price, &p.Quantity)
+		if err != nil {
+			s.logger.Error("Scanning row", zap.Error(err))
+			return nil, err
+		}
+
+		products = append(products, p)
+	}
+
+	if rows.Err() != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
